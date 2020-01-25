e2724b9c2e94cb0c290a07bd9e511e7e9154b560
Junaid Malik <jmalik981@gmail.com>
2020-01-25 21:30:28 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,18 +1,16 @@
 class Cat
-  # code goes here
-  attr_accessor :mood, :owner
-  attr_reader :name
+attr_accessor :mood, :owner
+attr_reader :name
 
-  @@all = []
+@@all = []
 
-  def initialize(name, owner)
-    @name = name
-    @mood = "nervous"
-    @owner = owner
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
+def initialize(name, owner)
+  @name = name
+  @mood = "nervous"
+  @owner = owner
+  @@all << self
+end
+def self.all
+  @@all
+end
 end
